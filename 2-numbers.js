913860de804b68e78eaccdd1724df10c36296af1
Steve Yang <steveyangwork@gmail.com>
2023-12-06 14:41:46 -0600

    update

--- 2-numbers.js
+++ 2-numbers.js
@@ -9,35 +9,39 @@ let divide = 5 / 2; // 2.5
 let modulo = 5 % 2; // 1
 
 // 2. Increment, Decrement 
-// i = i + 1;
+// i = i + 1
+// i++
 let myBankAccount = 0;
 myBankAccount = myBankAccount + 1; // 1
 myBankAccount++; // 1
 
 // console.log(myBankAccount); // 2
+myBankAccount = myBankAccount - 1; // -1
+myBankAccount--; // -1
 
 // 3. Augmented Assignment
-myBankAccount = 0;
-myBankAccount += 10;
-myBankAccount *= 5;
-myBankAccount /= 2;
-myBankAccount -= 30;
+myBankAccount += 10; // 10
+myBankAccount *= 5; // 50
+myBankAccount /= 2; // 25
+myBankAccount -= 30; // -5
+
+// console.log(myBankAccount); // -5
 
 // 4. Concatenation
-let sentence = 'I am walking ' + 'to the park.';
+let sentence = 'I am walking' + ' to the park.';
 // console.log(sentence);
 
+
 // 5. Methods: .length
-let greeting = 'Hello worldkjashdf;kasdklfja;lsdkfjl;askdjfl;aksdjf;laksjdf;lasjdf;laskjdf!'; //12
-let greetingLength = greeting.length; // ???
-console.log(greetingLength);
+let greeting = 'Hello world!'; // 12
+console.log(greeting.length); // 12
 
 // how does it know how to count?
 // zero-based indexing
 
-// console.log(greeting[2]); // 'Hel'
+console.log(greeting[0]); // 'H'
 
-// strings are immutable => cannot be changed
-let j = greeting[0] = 'J';
-greeting = 'Jello world'
+greeting[0] = 'J';
 console.log(greeting);
+
+//! Strings are Immutable => cannot be changed
