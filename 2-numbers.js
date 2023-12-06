425d4afbb52b46606b80e88b107915de4f3c1840
Steve Yang <steveyangwork@gmail.com>
2023-12-06 14:57:50 -0600

    update numbers

--- 2-numbers.js
+++ 2-numbers.js
@@ -9,39 +9,32 @@ let divide = 5 / 2; // 2.5
 let modulo = 5 % 2; // 1
 
 // 2. Increment, Decrement 
-// i = i + 1
-// i++
+// i = i + 1;
 let myBankAccount = 0;
 myBankAccount = myBankAccount + 1; // 1
 myBankAccount++; // 1
 
-// console.log(myBankAccount); // 2
-myBankAccount = myBankAccount - 1; // -1
-myBankAccount--; // -1
-
 // 3. Augmented Assignment
-myBankAccount += 10; // 10
-myBankAccount *= 5; // 50
-myBankAccount /= 2; // 25
-myBankAccount -= 30; // -5
-
-// console.log(myBankAccount); // -5
+myBankAccount += 10;
+myBankAccount *= 5;
+myBankAccount /= 2;
+myBankAccount -= 30;
 
 // 4. Concatenation
-let sentence = 'I am walking' + ' to the park.';
+let sentence = 'I am walking' + 'to the park.';
 // console.log(sentence);
 
-
 // 5. Methods: .length
-let greeting = 'Hello world!'; // 12
-console.log(greeting.length); // 12
+let greeting = 'Hello world'; // 12
+// console.log(greeting.length); // ???
 
 // how does it know how to count?
 // zero-based indexing
 
+// bracket notation = variable[index]
 console.log(greeting[0]); // 'H'
 
 greeting[0] = 'J';
-console.log(greeting);
+console.log(greeting); // "Jello world!"
 
-//! Strings are Immutable => cannot be changed
+//! Strings are IMMUTABLE => cannot be changed
